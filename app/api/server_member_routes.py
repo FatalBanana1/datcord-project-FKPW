60e08ab882988c4010194160482970c108c083fa
Fahd Ahsan <sfahsan@umich.edu>
2023-02-10 20:18:33 -0500

    get server members

--- app/api/server_member_routes.py
+++ app/api/server_member_routes.py
@@ -6,3 +6,10 @@ from app.models import Server
 #  url_prefix="/api/sms
 
 server_member_routes = Blueprint("server_members", __name__)
+
+@server_routes.route("/<int:server_id>/members")
+@login_required
+def all_server_members(server_id):
+    server = Server.query.get(server_id)
+    members = [member.to_dict() for member in server.server_members]
+    return {'server_members': members}
