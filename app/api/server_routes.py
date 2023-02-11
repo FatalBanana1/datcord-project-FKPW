312f7527fa53fdaccc0ccaf12de02da13fe84dba
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-10 21:48:40 -0800

    finished server routes i think

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -14,15 +14,17 @@ server_routes = Blueprint("servers", __name__)
 @login_required
 def servers():
     servers = Server.query.all()
-    return {"servers": [server.to_dict() for server in servers]}
+    return {"servers": [server.to_dict() for server in servers]}, 200
 
 # GET SERVERS BY USER ID
-@server_routes.route('/')
+@server_routes.route('/user')
 @login_required
 def users_servers():
-    servers = Server.query.filter(Server.server_members.any(id=current_user.id)).all()
-    return {"servers": [server.to_dict() for server in servers]}
-
+    # get list of all the server memberships to the current user
+    user_memberships = current_user.server_memberships
+    # looping through user_memberships to get that membership.server
+    servers = [membership.server for membership in user_memberships]
+    return {"servers": [server.to_dict() for server in servers]}, 200
 
 # CREATE SERVER
 @server_routes.route("/", methods=['POST'])
@@ -39,19 +41,32 @@ def create_server():
         )
         db.session.add(server)
         db.session.commit()
-        return {"server": server.to_dict()}
+        return {"server": server.to_dict()},
 
 # UPDATE SERVER
 @server_routes.route("/<int:id>", methods=['PUT'])
 @login_required
 def update_server(id):
-    pass
+    form = ServerForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+    server = Server.query.get(id)
+    if server:
+        server.name = form.data["name"]
+        server.icon_url = form.data["icon_url"]
+        server.description = form.data["description"]
+
+        db.session.commit()
+        return {"server": server.to_dict()}
 
 # DELETE SERVER
 @server_routes.route("<int:id>", methods=['DELETE'])
 @login_required
 def delete_server(id):
-    pass
+    server = Server.query.get(id)
+    if server:
+        db.session.delete(server)
+        db.session.commit()
+    return {"Response": "Successfully deleted server."}
 
 # @server_routes.route("/<int:id>")
 # @login_required
