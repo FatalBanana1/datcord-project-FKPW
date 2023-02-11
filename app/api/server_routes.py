886cf05c235f54e1da4eda4c0edba299996f59da
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-10 16:55:12 -0800

    set up server route functions

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -8,12 +8,33 @@ server_routes = Blueprint("servers", __name__)
 
 # /api/servers/:servers/channel
 
+# GET ALL SERVERS
 @server_routes.route("/")
 @login_required
 def servers():
-    data = Server.query.all()
-    return {"servers": [el.to_dict() for el in data]}
+    servers = Server.query.all()
+    return {"servers": [server.to_dict() for server in servers]}
 
+# GET SERVERS BY USER ID
+
+
+# CREATE SERVER
+@server_routes.route("/", methods=['POST'])
+@login_required
+def create_server():
+    pass
+
+# UPDATE SERVER
+@server_routes.route("/<int:id>", methods=['PUT'])
+@login_required
+def update_server(id):
+    pass
+
+# DELETE SERVER
+@server_routes.route("<int:id>", methods=['DELETE'])
+@login_required
+def delete_server(id):
+    pass
 
 # @server_routes.route("/<int:id>")
 # @login_required
