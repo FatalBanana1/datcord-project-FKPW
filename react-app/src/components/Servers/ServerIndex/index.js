ea4e0360961cabc9307930eeddbd6935c3aceb2c
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-14 13:45:23 -0800

    commented back in

--- react-app/src/components/Servers/ServerIndex/index.js
+++ react-app/src/components/Servers/ServerIndex/index.js
@@ -6,53 +6,53 @@ import background from "../../../assets/explore-servers-background.png";
 import { NavLink } from "react-router-dom";
 
 const ServerIndex = () => {
-	let dispatch = useDispatch();
-	let [isLoaded, setIsLoaded] = useState(false);
+  let dispatch = useDispatch();
+  let [isLoaded, setIsLoaded] = useState(false);
 
-	useEffect(() => {
-		dispatch(thunkReadAllServers()).then(() => setIsLoaded(true));
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(thunkReadAllServers()).then(() => setIsLoaded(true));
+  }, [dispatch]);
 
-	let allServers = useSelector((state) => state.servers);
+  let allServers = useSelector((state) => state.servers);
 
-	let servers = Object.values(allServers);
+  let servers = Object.values(allServers);
 
-	// console.log(`server index comp-----`, servers);
+  // console.log(`server index comp-----`, servers);
 
-	if (isLoaded) {
-		// return
-		return (
-			<div className="exp-bk">
-				<NavLink to="/channels/@me" className="exp-link">
-					Back to my channels
-				</NavLink>
-				<div className="exp-ct">
-					<div className="mtop-15">
-						<img className="exp-img" src={background} />
-					</div>
+  if (isLoaded) {
+    // return
+    return (
+      <div className="exp-bk">
+        <NavLink to="/channels/@me" className="exp-link">
+          Back to my channels
+        </NavLink>
+        <div className="exp-ct">
+          <div className="mtop-15">
+            <img className="exp-img" src={background} />
+          </div>
 
-					<div className="mtop-15">Featured Communities</div>
-					<div className="mtop-15 exp-servers-ct">
-						{servers.map((el) => (
-							<NavLink
-								to={`/channels/${el.id}/${el.channels[0].id}`}
-								key={el.id}
-								className="mtop-15 exp-serv"
-							>
-								<img
-									src={el.icon_url}
-									className="servers-icon"
-									alt="server icon"
-								/>
-								<div>{`Server : ${el.name}`}</div>
-								<div>{`About : ${el.description}`}</div>
-							</NavLink>
-						))}
-					</div>
-				</div>
-			</div>
-		);
-	} else return <div>Loading...</div>;
+          <div className="mtop-15">Featured Communities</div>
+          <div className="mtop-15 exp-servers-ct">
+            {servers.map((el) => (
+              <NavLink
+                to={`/channels/${el.id}/${el.channels[0].id}`}
+                key={el.id}
+                className="mtop-15 exp-serv"
+              >
+                <img
+                  src={el.icon_url}
+                  className="servers-icon"
+                  alt="server icon"
+                />
+                <div>{`Server : ${el.name}`}</div>
+                <div>{`About : ${el.description}`}</div>
+              </NavLink>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  } else return <div>Loading...</div>;
 };
 
 export default ServerIndex;
