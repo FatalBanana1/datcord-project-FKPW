e4cd2a9ebd266a6013e05a6b51cb349e1e4bb37b
fatalbanana <dark1knite@gmail.com>
2023-02-14 16:27:09 -0500

    gotmilk pg get/links

--- react-app/src/components/Servers/ServerIndex/index.js
+++ react-app/src/components/Servers/ServerIndex/index.js
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkReadAllServers } from "../../../store/servers";
 import "./ServerIndex.css";
+import background from "../../../assets/explore-servers-background.png";
+import { NavLink } from "react-router-dom";
 
 const ServerIndex = () => {
 	let dispatch = useDispatch();
@@ -19,21 +21,33 @@ const ServerIndex = () => {
 
 	// return
 	return (
-		<div>
-			<div className="mtop-15">Servers comp</div>
-			<div className="mtop-15">List of Servers:</div>
-			<div className="mtop-15">
-				{servers.map((el) => (
-					<div key={el.id} className="mtop-15">
-						<img
-							src={el.icon_url}
-							className="servers-icon"
-							alt="server icon"
-						/>
-						<div>{`Server : ${el.name}`}</div>
-						<div>{`About : ${el.description}`}</div>
-					</div>
-				))}
+		<div className="exp-bk">
+			<NavLink to="/channels/@me" className="exp-link">
+				Back to my channels
+			</NavLink>
+			<div className="exp-ct">
+				<div className="mtop-15">
+					<img className="exp-img" src={background} />
+				</div>
+
+				<div className="mtop-15">Featured Communities</div>
+				<div className="mtop-15 exp-servers-ct">
+					{servers.map((el) => (
+						<NavLink
+							to={`/channels/${el.id}/${el.channels[0].id}`}
+							key={el.id}
+							className="mtop-15 exp-serv"
+						>
+							<img
+								src={el.icon_url}
+								className="servers-icon"
+								alt="server icon"
+							/>
+							<div>{`Server : ${el.name}`}</div>
+							<div>{`About : ${el.description}`}</div>
+						</NavLink>
+					))}
+				</div>
 			</div>
 		</div>
 	);
