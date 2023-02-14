42c28479e8435ecab797f02c54fbc64021a3811c
fatalbanana <dark1knite@gmail.com>
2023-02-14 16:34:41 -0500

    add loading to gotmilk

--- react-app/src/components/Servers/ServerIndex/index.js
+++ react-app/src/components/Servers/ServerIndex/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkReadAllServers } from "../../../store/servers";
 import "./ServerIndex.css";
@@ -7,10 +7,10 @@ import { NavLink } from "react-router-dom";
 
 const ServerIndex = () => {
 	let dispatch = useDispatch();
-	// let [isLoaded, setIsLoaded] = useState(false);
+	let [isLoaded, setIsLoaded] = useState(false);
 
 	useEffect(() => {
-		dispatch(thunkReadAllServers());
+		dispatch(thunkReadAllServers()).then(() => setIsLoaded(true));
 	}, [dispatch]);
 
 	let allServers = useSelector((state) => state.servers);
@@ -19,38 +19,40 @@ const ServerIndex = () => {
 
 	// console.log(`server index comp-----`, servers);
 
-	// return
-	return (
-		<div className="exp-bk">
-			<NavLink to="/channels/@me" className="exp-link">
-				Back to my channels
-			</NavLink>
-			<div className="exp-ct">
-				<div className="mtop-15">
-					<img className="exp-img" src={background} />
-				</div>
+	if (isLoaded) {
+		// return
+		return (
+			<div className="exp-bk">
+				<NavLink to="/channels/@me" className="exp-link">
+					Back to my channels
+				</NavLink>
+				<div className="exp-ct">
+					<div className="mtop-15">
+						<img className="exp-img" src={background} />
+					</div>
 
-				<div className="mtop-15">Featured Communities</div>
-				<div className="mtop-15 exp-servers-ct">
-					{servers.map((el) => (
-						<NavLink
-							to={`/channels/${el.id}/${el.channels[0].id}`}
-							key={el.id}
-							className="mtop-15 exp-serv"
-						>
-							<img
-								src={el.icon_url}
-								className="servers-icon"
-								alt="server icon"
-							/>
-							<div>{`Server : ${el.name}`}</div>
-							<div>{`About : ${el.description}`}</div>
-						</NavLink>
-					))}
+					<div className="mtop-15">Featured Communities</div>
+					<div className="mtop-15 exp-servers-ct">
+						{servers.map((el) => (
+							<NavLink
+								to={`/channels/${el.id}/${el.channels[0].id}`}
+								key={el.id}
+								className="mtop-15 exp-serv"
+							>
+								<img
+									src={el.icon_url}
+									className="servers-icon"
+									alt="server icon"
+								/>
+								<div>{`Server : ${el.name}`}</div>
+								<div>{`About : ${el.description}`}</div>
+							</NavLink>
+						))}
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	} else return <div>Loading...</div>;
 };
 
 export default ServerIndex;
