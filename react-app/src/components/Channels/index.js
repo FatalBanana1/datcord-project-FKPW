333750dcf6a492aeeca8460054950ed83863df4c
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-14 10:19:49 -0800

    resolved

--- react-app/src/components/Channels/index.js
+++ react-app/src/components/Channels/index.js
@@ -15,37 +15,77 @@ import DeleteServer from "../Servers/DeleteServer";
 import { thunkGetServerMembers } from "../../store/serverMembers";
 
 export default function Channels() {
-    const { serverId, channelId } = useParams();
-    // console.log("Channels - serverId, channelId:", serverId, channelId);
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.session.user);
-    // console.log("USER:", user);
-    // const server = useSelector(state => state.servers)[+serverId]
-    // const server = useSelector((state) => state.servers.userServers);
-    const server = useSelector((state) => state.channels.server);
-    // console.log("userServers", server)
-    // console.log("server we want:", server.find((thing) => thing.id === +serverId))
-    const channels = Object.values(useSelector((state) => state.channels.channels));
-    // console.log("SERVER", server);
-    const serverMembers = Object.values(useSelector((state) => state.serverMembers));
-    const serverMember = serverMembers.filter(member => member.user_id === user.id)[0];
-    // console.log("channels - serverMember:", serverMember);
+  const { serverId, channelId } = useParams();
+  console.log("Channels - serverId, channelId:", serverId, channelId);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  console.log("USER:", user);
+  // const server = useSelector(state => state.servers)[+serverId]
+  // const server = useSelector((state) => state.servers.userServers);
+  const server = useSelector((state) => state.channels.server);
+  console.log("userServers", server);
+  // console.log("server we want:", server.find((thing) => thing.id === +serverId))
+  const channels = Object.values(
+    useSelector((state) => state.channels.channels)
+  );
+  // console.log("SERVER", server);
+  const serverMembers = Object.values(
+    useSelector((state) => state.serverMembers)
+  );
+  const serverMember = serverMembers.filter(
+    (member) => member.user_id === user.id
+  )[0];
+  // console.log("channels - serverMember:", serverMember);
 
-    let serverMemberRole;
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
 
-    let permissions;
+  let serverMemberRole;
 
-    // console.log("USER", user);
-    const [showMenu, setShowMenu] = useState(false);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [showEdit, setShowEdit] = useState(false);
+  let permissions;
 
-    // console.log("show edit", showEdit);
+  console.log(
+    "SERVER_CHANNELS",
+    channels.filter((channel) => channel.id === 1)
+  );
+  console.log("USER", user);
+  const [showMenu, setShowMenu] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+  const [showServerMenu, setShowServerMenu] = useState(false);
+  const menuRef = useRef();
+
+  // PETER'S SERVER DROP DOWN STUFF
+  let isOwner;
+  if (isLoaded) {
+    isOwner = user.id == server.owner_id;
+  }
+
+  const openServerMenu = () => {
+    if (showServerMenu) return;
+    setShowServerMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showServerMenu) return;
+
+    const closeServerMenu = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setShowServerMenu(false);
+      }
+    };
+
+    document.addEventListener("click", closeServerMenu);
 
     return () => document.removeEventListener("click", closeServerMenu);
   }, [showServerMenu]);
 
-    const categories = {};
+  const menuClassName =
+    "server-menu-dropdown" + (showServerMenu ? "" : " hidden");
+
+  console.log("show edit", showEdit);
 
   useEffect(() => {
     dispatch(thunkGetServerMembers(+serverId));
@@ -87,10 +127,9 @@ export default function Channels() {
     }
   }
 
-    if (serverMemberRole === "admin" || serverMemberRole === "owner") {
-        permissions = true;
-    } else {
-        permissions = false;
+  const truncateNames = (names) => {
+    if (names.length > 18) {
+      return `${names.substring(0, 18)}...`;
     }
 
     return names;
@@ -136,7 +175,6 @@ export default function Channels() {
   console.log("permissions", permissions);
   console.log("serverMember", serverMember);
 
-<<<<<<< HEAD
   const categoriesMap = Object.keys(categories).map((category, idx) => (
     <div className="UserLanding-Sidebar-category-container" key={idx}>
       <div className="UserLanding-sidebar-channel-category-container">
@@ -219,95 +257,6 @@ export default function Channels() {
               <i className="fa-solid fa-microphone"></i>
               <i className="fa-solid fa-headphones"></i>
               <i className="fa-solid fa-gear user-gear"></i>
-=======
-    // const categoriesMap = Object.keys(categories).map((category) => (
-    // 	<>
-    // 		<div className="UserLanding-sidebar-channel-category-container">
-    // 			<i className="fa-solid fa-angle-down"></i>
-    // 			<span className="UserLanding-sidebar-channel-category-name">
-    // 				{truncateNames(category)}
-    // 			</span>
-    // 			<i className="fa-solid fa-plus align-right"></i>
-    // 		</div>
-    // 		<div className="UserLanding-sidebar-channel-list">
-    // 			{/* map out channels here */}
-    // 			{category &&
-    // 				categories[category].map((channel) => (
-    // 					<NavLink
-    // 						to={`/channels/2/3`}
-    // 						className="UserLanding-sidebar-channel-name"
-    // 						key={channel.id}
-    // 					>
-    // 						<div className="UserLanding-sidebar-channel-name-label">
-    // 							<span className="hash">#</span>{" "}
-    // 							{channel.name && truncateNames(channel.name)}
-    // 						</div>
-    // 						{/* if admin, then show these buttons v */}
-    // 						<div className="UserLanding-sidebar-channel-buttons">
-    // 							<i className="fa-solid fa-user-plus"></i>
-    // 							<i className="fa-solid fa-gear"></i>
-    // 						</div>
-    // 					</NavLink>
-    // 				))}
-    // 		</div>
-    // 	</>
-    // ));
-
-    const closeMenu = () => setShowMenu(false);
-
-    // console.log("channels", channels);
-    // console.log("categories", categories.category);
-    // console.log("permissions", permissions);
-    // console.log("serverMember", serverMember);
-
-    const categoriesMap = Object.keys(categories).map((category,idx) => (
-        <div className="UserLanding-Sidebar-category-container" key={idx}>
-            <div className="UserLanding-sidebar-channel-category-container">
-                <i className="fa-solid fa-angle-down"></i>
-                <span className="UserLanding-sidebar-channel-category-name">{truncateNames(category)}</span>
-                { permissions && (
-                    <OpenModalButton
-                        buttonText="Create-Channel"
-                        onButtonClick={closeMenu}
-                        modalComponent={<CreateChannelForm categoryName={category} serverId={serverId} />}
-                    />
-                )}
-            </div>
-            <div className="UserLanding-sidebar-channel-list">
-                {/* map out channels here */}
-                {category &&
-                    channels.length > 0 &&
-                    categories[category].map((channel) => (
-                        <NavLink
-                            to={`/channels/${serverId}/${channel.id}`}
-                            className="UserLanding-sidebar-channel-name"
-                            key={channel.id}
-                        >
-                            <div className="UserLanding-sidebar-channel-name-label">
-                                <span className="hash">#</span> {channel.name && truncateNames(channel.name)}
-                            </div>
-                            { permissions && (
-                                <div className="UserLanding-sidebar-channel-buttons">
-                                    <i className="fa-solid fa-user-plus"></i>
-                                    {/* <NavLink to={`/channels/${serverId}/${channel.id}/edit`}>
-                                        <i className="fa-solid fa-gear" onClick={() => setShowEdit(true)}></i>
-                                    </NavLink> */}
-                                    <OpenModalButton
-                                        buttonText="Edit-Channel"
-                                        onButtonClick={closeMenu}
-                                        modalComponent={<EditChannelForm
-                                        categoryName={category}
-                                        prevName={channel.name}
-                                        serverId={serverId}
-                                        channelId={channel.id}
-                                        priv={channel.is_private}
-                                        />}
-                                    />
-                                </div>
-                            )}
-                        </NavLink>
-                    ))}
->>>>>>> dev
             </div>
           </div>
         </div>
