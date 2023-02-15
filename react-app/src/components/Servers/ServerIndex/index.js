8ad81c9ec13340916fb1efd55439ccbc1ebb29d2
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-14 16:05:08 -0800

    working on got milk

--- react-app/src/components/Servers/ServerIndex/index.js
+++ react-app/src/components/Servers/ServerIndex/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkReadAllServers } from "../../../store/servers";
 import "./ServerIndex.css";
-import background from "../../../assets/explore-servers-background.png";
+import explorerBanner from "../../../assets/explorer-banner.svg";
 import { NavLink } from "react-router-dom";
 import ServerNav from "../../MainPage/ServerNav";
 
@@ -15,7 +15,7 @@ const ServerIndex = () => {
 
   useEffect(() => {
     dispatch(thunkReadAllServers()).then(() => setIsLoaded(true));
-  }, [dispatch]);
+  }, [dispatch, isLoaded]);
 
   if (isLoaded) {
     let servers = Object.values(allServers);
@@ -28,13 +28,13 @@ const ServerIndex = () => {
         {/* <div to="/channels/@me" className="exp-link">
 				</div> */}
         <ServerNav />
-        <div className="exp-ct">
-          <div className="mtop-15">
-            <img className="exp-img" src={background} />
+        <div className="explorer-container">
+          <div className="explorer-banner-container">
+            <img className="explorer-img" src={explorerBanner} />
           </div>
 
-          <div className="mtop-15">Featured Communities</div>
-          <div className="mtop-15 exp-servers-ct">
+          <div className="explorer-banner-header">Featured Communities</div>
+          <div className="explorer-banner-server-container">
             {servers.map((el) => {
               if (
                 el &&
@@ -43,39 +43,39 @@ const ServerIndex = () => {
                 user.server_members.find((j) => j.server_id === el.id)
               ) {
                 return (
-                  <NavLink
-                    to={`/channels/${el.id}/${el.channels[0].id}`}
-                    key={el.id}
-                    className="mtop-15 exp-serv"
-                  >
-                    <img
-                      src={el.icon_url}
-                      className="servers-icon"
-                      alt="server icon"
-                    />
-                    <div>{`Server : ${el.name}`}</div>
-                    <div>{`About : ${el.description}`}</div>
-                    <div>Already Joined</div>
-                    <div>End of card</div>
-                  </NavLink>
+                  <div className="explorer-server-container">
+                    <NavLink
+                      to={`/channels/${el.id}/${el.channels[0].id}`}
+                      key={el.id}
+                      className=""
+                    >
+                      <img
+                        src={el.icon_url}
+                        className="servers-icon"
+                        alt="server icon"
+                      />
+                      <div>{`Server : ${el.name}`}</div>
+                      <div>{`About : ${el.description}`}</div>
+                      <div>Already Joined</div>
+                      <div>End of card</div>
+                    </NavLink>
+                  </div>
                 );
               } else {
                 return (
-                  <NavLink
-                    to={`/channels/@me/`}
-                    key={el.id}
-                    className="mtop-15 exp-serv"
-                  >
-                    <img
-                      src={el.icon_url}
-                      className="servers-icon"
-                      alt="server icon"
-                    />
-                    <div>{`Server : ${el.name}`}</div>
-                    <div>{`About : ${el.description}`}</div>
-                    <div>Join Server</div>
-                    <div>End of card</div>
-                  </NavLink>
+                  <div className="explorer-server-container">
+                    <NavLink to={`/channels/@me/`} key={el.id} className="">
+                      <img
+                        src={el.icon_url}
+                        className="servers-icon"
+                        alt="server icon"
+                      />
+                      <div>{`Server : ${el.name}`}</div>
+                      <div>{`About : ${el.description}`}</div>
+                      <div>Join Server</div>
+                      <div>End of card</div>
+                    </NavLink>
+                  </div>
                 );
               }
             })}
