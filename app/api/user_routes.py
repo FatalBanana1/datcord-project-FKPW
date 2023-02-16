816f86ae447824fe45db352b672ca8a0edad5558
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-16 12:40:01 -0800

    started themes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -23,3 +23,12 @@ def user(id):
     """
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/<int:id>', methods=["POST"])
+@login_required
+def user(id):
+    """
+    Query for a user by id and returns that user in a dictionary
+    """
+    user = User.query.get(id)
+    return user.to_dict()
