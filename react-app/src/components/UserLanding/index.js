0841595b50de6c613068209d1980d2849350ffcb
Peter Nguyen <nguyen.peter.viet@gmail.com>
2023-02-16 16:52:56 -0800

    finished userlanding light theme

--- react-app/src/components/UserLanding/index.js
+++ react-app/src/components/UserLanding/index.js
@@ -5,32 +5,34 @@ import ServerMembers from "../ServerMembers";
 import "./UserLanding.css";
 
 export default function UserLanding({ page, theme }) {
-	const user = useSelector((state) => state.session.user);
+  const user = useSelector((state) => state.session.user);
 
-	switch (page) {
-		case "channel": {
-			return theme && (
-				<div className="UserLanding-container reverse" id={theme}>
-					{/* <div className="UserLanding-main-content"> */}
-					{/* <div className="UserLanding-server-members"> */}
-					<ServerMembers theme={theme} />
-					<CMIndex theme={theme} />
-					{/* </div> */}
-					{/* </div> */}
-				</div>
-			);
-		}
-		default: {
-			return (
-				<div className="UserLanding-container">
-					{/* <ol>
+  switch (page) {
+    case "channel": {
+      return (
+        theme && (
+          <div className="UserLanding-container reverse" id={theme}>
+            {/* <div className="UserLanding-main-content"> */}
+            {/* <div className="UserLanding-server-members"> */}
+            <ServerMembers theme={theme} />
+            <CMIndex theme={theme} />
+            {/* </div> */}
+            {/* </div> */}
+          </div>
+        )
+      );
+    }
+    default: {
+      return (
+        <div className="UserLanding-container">
+          {/* <ol>
                 <li>welcome page when first logging in</li>
                 <li>messages if on server page</li>
                 <li>members list if on server page</li>
             </ol> */}
-					<FriendshipsPage theme={theme} />
-				</div>
-			);
-		}
-	}
+          <FriendshipsPage theme={theme} />
+        </div>
+      );
+    }
+  }
 }
