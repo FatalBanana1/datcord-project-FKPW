7598967a58fc7b661fc0ae5a490448753e5cef4d
fatalbanana <dark1knite@gmail.com>
2023-02-19 19:45:12 -0500

    add-aws

--- app/aws_s3_upload.py
+++ app/aws_s3_upload.py
@@ -7,11 +7,7 @@ BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
 ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif", "svg"}
 
-# s3 = boto3.client(
-#     "s3",
-#     aws_access_key_id=os.environ.get("S3_KEY"),
-#     aws_secret_access_key=os.environ.get("S3_SECRET"),
-# )
+s3 = boto3.client("s3")
 
 
 def allowed_file(filename):
